a3c7b026d82aada70e4b1fca4553cd3026c59ca7
ildnyy <2114829840@qq.com>
2019-08-20 17:38:52 +0800

    feat

---
+++ src/compoment/service.js
@@ -0,0 +1,102 @@
+function Fetch(url, opt = {}) {
+    opt.method = opt.method || "GET";
+    opt.headers = {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    };
+    if (opt.token) {
+      opt.headers.token = opt.token;
+    }
+  
+    opt.body = JSON.stringify(opt.data) || null;
+  
+    return fetch(url, opt).then(response => {
+      return response.json().then(json => {
+        switch (response.status) {
+        //   case 200:
+        //     return json;
+        //   case 502:
+        //     util.message(response.statusText, "err");
+        //   case 403:
+        //     util.message(json.message, "err");
+        //   case 401:
+        //     return response.status;
+        //     break;
+        }
+      });
+    });
+  }
+  
+  let Service = {
+    // login
+    Login(username, password) {
+      return Fetch("http://pass.muxixyz.com/api/login/", {
+        method: 'POST',
+        data: {
+          username: username,
+          password: btoa(password)
+        }
+      })
+    },
+    // register 
+    register(email, username, password) {
+      return Fetch("http://pass.muxixyz.com/api/signup/", {
+        method: "POST",
+        data: {
+          email: email,
+          username: username,
+          password: password
+        }
+      })
+    },
+    // check email
+    checkEmail(email) {
+      return fetch(`/api/check_email/?email=${email}`)
+    },
+    // check username
+    checkUsername(username) {
+      return fetch(`/api/check_name/?username=${username}`)
+    },
+    // reset password
+    resetPassword(email, password, captcha) {
+      return Fetch("http://pass.muxixyz.com/api/password/reset", {
+        method: "POST",
+        data: {
+          new_password: password,
+          email: email,
+          captcha: captcha
+        }
+      })
+    },
+    // get captcha
+    getCaptcha(email) {
+      return Fetch("http://pass.muxixyz.com/api/password/get_captcha/", {
+        method: "POST",
+        data: {
+          email: email
+        }
+      })
+    },
+    // check captcha
+    checkCaptcha(email, captcha) {
+      return Fetch("http://pass.muxixyz.com/api/password/check_captcha/", {
+        method: "POST",
+        data: {
+          email: email,
+          captcha: captcha
+        }
+      })
+    },
+    // get profile
+    getProfile(id, token) {
+      return Fetch("http://pass.muxixyz.com/api/show_profile/"+id, {
+          token: token
+      })
+    },
+    // get user's share
+    getShare(id) {
+      return Fetch("http://share.muxixyz.com/api/v2.0/get_one_all/" + id + "/")
+    }
+  };
+  
+  export default Service;
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import './login.css';
 import Background from '../../images/login.png';
-import Post from '../../compoment/request'
+import Service from '../../compoment/service'
 var sectionStyle = {
     width: "100%",
     padding: "0 0 0 0",
@@ -12,40 +12,46 @@ var sectionStyle = {
   };
 
 class Login extends Component {
-    state = {
-        username:'',
-        email:'',
-        f_password:'',
-        s_password:''
-    }
+    constructor(props){
+        super(props);
+        this.state = {
+            username:'',
+            email:'',
+            f_password:'',
+            s_password:''
+    }}
     ChangeUsername(e) {
         this.setState({
-          username: e.detail.value
-        });
+            username:e.target.value
+        })
     }
     ChangeEmail(e) {
         this.setState({
-          email: e.detail.value
+          email: e.target.value
         });
     } 
     ChangeFPassword(e) {
         this.setState({
-          f_password: e.detail.value
+          f_password: e.target.value
         });
     }
     ChangeSPassword(e) {
         this.setState({
-          s_password: e.detail.value
+          s_password: e.target.value
         });
     }
     login(){
-        Post('',{
-
-        }).then((res)=>{
-            if(res){
-                
-            }           
-        })
+        Service.register(this.state.email , this.state.username, this.state.s_password).then(res=>{
+            if (res !== null && res!== undefined) {
+                let landing = localStorage.getItem('landing')
+                if (landing) {
+                    window.location.href = 'http://'+ landing + '?username=' + this.username +'&token=' + res.token + '&id=' + res.user_id
+                }
+            } else {
+                this.failed = true
+            }
+            }
+        )
     }
     render() {
         const { username , email , f_password , s_password}= this.state
@@ -63,32 +69,32 @@ class Login extends Component {
                                 <input type="text"
                                     placeholder="用户名"
                                     value={username}
-                                    onInput={this.ChangeUsername}
-                                    onChange={this.ChangeUsername}
+                                    onInput={this.ChangeUsername.bind(this)}
+                                    onChange={this.ChangeUsername.bind(this)}
                                     className="user_nickname" />
                             </div>
                             <div className="input-prepend" >
                                 <input type="email"
                                     placeholder="登录邮箱"
                                     value={email}
-                                    onInput={this.ChangeEmail}
-                                    onChange={this.ChangeEmail}
+                                    onInput={this.ChangeEmail.bind(this)}
+                                    onChange={this.ChangeEmail.bind(this)}
                                     className="user_email_number" />
                             </div>
                             <div className="input-prepend" >
                                 <input type="password"
                                     placeholder="密码"
                                     value={f_password}
-                                    onInput={this.ChangeFPassword}
-                                    onChange={this.ChangeFPassword}
+                                    onInput={this.ChangeFPassword.bind(this)}
+                                    onChange={this.ChangeFPassword.bind(this)}
                                     className="user_password" />
                             </div>
                             <div className="input-prepend" >
                                 <input type="password"
                                     placeholder="确认密码"
                                     value={s_password}
-                                    onInput={this.ChangeSPassword}
-                                    onChange={this.ChangeSPassword}
+                                    onInput={this.ChangeSPassword.bind(this)}
+                                    onChange={this.ChangeSPassword.bind(this)}
                                     className="user_confi_password" />
                             </div>
                             <div className="auto-login-btn" >
@@ -98,7 +104,7 @@ class Login extends Component {
                                     name="session[auto_login]"
                                     className="session_auto_login" />  <span > 下次自动登陆 </span>
                             </div>
-                            <button className="sign-in-button" type="button" onClick={this.login} > 开启新世界大门 </button>
+                            <button className="sign-in-button" type="button" onClick={this.login.bind(this)} > 开启新世界大门 </button>
                             <p className="sign-in-msg" > 以上设置可在页面右上角个人信息中再次修改 </p>
 
                         </form>

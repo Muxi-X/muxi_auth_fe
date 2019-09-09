ead4bc7838e4223a82f8d610f5af2af209a9265e
jonusis <jonusis@github.com>
2019-09-09 20:21:26 +0800

    feat:fix some mistakes in grammer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './common/index';
+import React, { Component } from 'react';
+import {
+  HashRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
+import reset from './pages/reset/index';
+import Login from './pages/login/login';
+import Register from './pages/register/register';
 import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export default class Portal extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Redirect exact from="/" to="/login" />
+          <Route path="/Register" component={Register} />
+          <Route path="/find_pass" component={reset} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </Router>
+    );
+  }
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(<Portal />, document.getElementById('root'));
 serviceWorker.unregister();
